2bafa409b6ea79e3e930fee4854e287df92a2165
Haroon Ghawsi <haroon.ghawsi@ms.ingenico.com>
2021-12-29 02:32:03 +0100

    fix(GameService): fixed business logic

--- backend/src/main/java/com/demo/game/service/GameService.java
+++ backend/src/main/java/com/demo/game/service/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.demo.game.dao.GameOption;
 import com.demo.game.dao.GameResult;
+import com.demo.game.dto.GameResponseDto;
 import com.demo.game.repository.GameRepository;
 
 @Service
@@ -24,21 +25,60 @@ public class GameService {
     return repository.findAll();
   }
 
-  public GameResult processGameResult(GameOption userOption, GameOption systemOption) {
+  public GameResponseDto processGameResult(GameOption userOption) {
 
     LOGGER.info("Processing the final Result of the Game...");
 
-    if (userOption == null || systemOption == null) {
-      throw new IllegalArgumentException();
+    if (userOption == null) {
+      throw new IllegalGameOptionException("User Choice should not be null");
     }
-    if (userOption.equals(systemOption)) {
-      return GameResult.LOST;
+
+    GameOption opponentChoice = this.getOpponentChoice();
+
+    if (opponentChoice == null) {
+      throw new IllegalGameOptionException("Opponent Choice should not be null");
     }
-    return GameResult.WON;
+
+    if (userOption == GameOption.STONE && opponentChoice == GameOption.STONE) {
+      return GameResponseDto.from(opponentChoice, GameResult.DRAW);
+    }
+
+    if (userOption == GameOption.STONE && opponentChoice == GameOption.SCISSOR) {
+      return GameResponseDto.from(opponentChoice, GameResult.WON);
+    }
+
+    if (userOption == GameOption.STONE && opponentChoice == GameOption.PAPER) {
+      return GameResponseDto.from(opponentChoice, GameResult.LOST);
+    }
+
+    if (userOption == GameOption.SCISSOR && opponentChoice == GameOption.STONE) {
+      return GameResponseDto.from(opponentChoice, GameResult.LOST);
+    }
+
+    if (userOption == GameOption.SCISSOR && opponentChoice == GameOption.SCISSOR) {
+      return GameResponseDto.from(opponentChoice, GameResult.DRAW);
+    }
+
+    if (userOption == GameOption.SCISSOR && opponentChoice == GameOption.PAPER) {
+      return GameResponseDto.from(opponentChoice, GameResult.WON);
+    }
+
+    if (userOption == GameOption.PAPER && opponentChoice == GameOption.STONE) {
+      return GameResponseDto.from(opponentChoice, GameResult.WON);
+    }
+
+    if (userOption == GameOption.PAPER && opponentChoice == GameOption.SCISSOR) {
+      return GameResponseDto.from(opponentChoice, GameResult.LOST);
+    }
+
+    if (userOption == GameOption.PAPER && opponentChoice == GameOption.PAPER) {
+      return GameResponseDto.from(opponentChoice, GameResult.DRAW);
+    }
+
+    return GameResponseDto.from(opponentChoice, GameResult.WON);
   }
 
-
-  public GameOption getOpponentChoice() {
+  protected GameOption getOpponentChoice() {
     LOGGER.info("System started selecting Game Options");
     return repository.generateOptionForSystem();
   }
